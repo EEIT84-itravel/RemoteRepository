c1376cd0388ca5f64347b5a0d55611101c3c036a
fox <fox@III-EEIT>
2016-03-17 11:16:27 +0800

    modify web.xml,Sevrlet3.java

--- Proj01Web/src/controller/Servlet3.java
+++ Proj01Web/src/controller/Servlet3.java
@@ -7,14 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/Servlet03")
 public class Servlet3 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("AAA");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
